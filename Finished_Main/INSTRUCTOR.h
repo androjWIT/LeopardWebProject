edbcbaaec602aebb67f3ab9a796e6b4972b382b2
androjWIT <77814951+androjWIT@users.noreply.github.com>
2025-07-07 22:40:05 -0400

    Updated instructor code

--- Finished_Main/INSTRUCTOR.h
+++ Finished_Main/INSTRUCTOR.h
@@ -1,26 +1,23 @@
-
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-#include <sqlite3.h>
-#include <string>
 
+#include <string>
+#include <sqlite3.h>
 
 class INSTRUCTOR {
-    private:
-        sqlite3* db;
-        std::string instructorID;
+private:
+    sqlite3* db;
+    std::string instructorID;
 
-    public:
-        INSTRUCTOR(sqlite3* database) { db = database;}
-        void menu();
-        void searchCourse();
-        void searchCourseParam();
-        void printSchedule();
-        void searchRoster();
-        void printRoster();
-
+public:
+    INSTRUCTOR(sqlite3* dbConn);
+    void menu();
+    void searchCourse();
+    void searchCourseParam();
+    void printSchedule();
+    void searchRoster();
+    void printRoster();
 };
-
 
 
 #endif //INSTRUCTOR_H
